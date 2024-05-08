d9c13b25c8a77832c1a4f835e802fc55d22ca9c8
eternityscan <168107350+eternityscan@users.noreply.github.com>
2024-05-08 23:35:17 +0530

    Update obj.js

--- obj.js
+++ obj.js
@@ -1,17 +1,8 @@
-// Function to hash a string using SHA-256 algorithm
-function sha256(input) {
-  const crypto = require('crypto');
-  return crypto.createHash('sha256').update(input).digest('hex');
-}
-
 function checkPasscode() {
   var passcodeInput = document.getElementById("passcode").value;
-  var storedPasscodeHash = "fbfbab2ec07e82efc789c277b3dce1ee7e21b1102b6fe3c623b6e8fbcf5a3e2c"; // Hashed value of "4370"
+  var passcode = "4370";
 
-  // Hash the user input
-  var passcodeInputHash = sha256(passcodeInput);
-
-  if (passcodeInputHash === storedPasscodeHash) {
+  if (passcodeInput === passcode) {
     // If passcode is correct, display the web content
     document.querySelector('.passcode-form').style.display = 'none';
     document.querySelector('.web-content').style.display = 'block';
@@ -20,3 +11,6 @@ function checkPasscode() {
     alert("Access denied! Your worthiness remains unseen. Incorrect passcode.");
   }
 }
+
+
+
