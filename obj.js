931dbdd4f331ec1ffac4da7409dcaf04f08bae91
eternityscan <168107350+eternityscan@users.noreply.github.com>
2024-05-08 23:29:56 +0530

    Update obj.js

--- obj.js
+++ obj.js
@@ -1,13 +1,22 @@
+
+function sha256(input) {
+  const crypto = require('crypto');
+  return crypto.createHash('sha256').update(input).digest('hex');
+}
+
 function checkPasscode() {
   var passcodeInput = document.getElementById("passcode").value;
-  var passcode = "4370";
+  var storedPasscodeHash = "f75443d49e2b124e49d0cb6f12d38e2b41de5ad1d6929a28a6c56d03e07ad2c0"; 
 
-  if (passcodeInput === passcode) {
+
+  var passcodeInputHash = sha256(passcodeInput);
+
+  if (passcodeInputHash === storedPasscodeHash) {
     // If passcode is correct, display the web content
     document.querySelector('.passcode-form').style.display = 'none';
     document.querySelector('.web-content').style.display = 'block';
   } else {
-    // If passcode is incorrect, show a warning message
+
     alert("Access denied! Your worthiness remains unseen. Incorrect passcode.");
   }
 }
